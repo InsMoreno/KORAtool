4f35ae3d24ee0a98bfe69703539c372fed78600c
CrocutaLupus <luclegrandch@gmail.com>
2020-08-06 16:22:08 +0200

    EvCountR correction

--- R/EvCountR.R
+++ R/EvCountR.R
@@ -1,17 +1,13 @@
-#' Event Counting
-#'
+#' EvCountR
 #'
 #'This function creates a table with the number of event for a given species.
-#'Input needed:
-#'    - KORA.Photo.Output = "Nameofcsv.csv" KORA ouput with lynx or any other species observed (All have been IDed)
-#'    Important Note:
-#'    - The csv must be in the folder of the used RStudio Project
-#'    - Date in the csv must be in '%Y-%m-%d %H:%M:%S' (Standard KORA data and R format)
 #'
-#' @param # Author: Luc Le Grand
+#' @param KORA.Photo.Output Name of the object in R consol. Should be the KORA ouput with lynx or any other species observed (All have been IDed)
+#' @param Event_length
+#' @param species
+#'
 #' @return
-#' @export
-EvCount<-function(
+EvCountR<-function(
   KORA.Photo.Output,
   Event_length,
   species
@@ -19,12 +15,12 @@ EvCount<-function(
   # ---- Import Data ####
 
   # Import KORA Photo data
-  M.Table<-data.table::fread(KORA.Photo.Output, select=c(
+  M.Table<-data.table(KORA.Photo.Output[,c(
     "animal_species",
     "exposure_date",
     "exposure_time",
     "id_individual",
-    "id_flank"))
+    "id_flank")])
 
   # ---- Arange Master Table ####
 
@@ -39,7 +35,7 @@ EvCount<-function(
 
   for(i in 1:length(Table$ID)){
     df<-data.table(timestamp = M.Table[M.Table$animal_species==species &
-                                           M.Table$id_individual == Table[i, ID],TIME])
+                                         M.Table$id_individual == Table[i, ID],TIME])
     df<-df[, .(
       startTime = timestamp[1L],
       endTime = timestamp[.N],
@@ -61,5 +57,3 @@ EvCount<-function(
 
 
 }
-
-
