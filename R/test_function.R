ab5b12650dda6218a61cfacf9e3c1f4f78a2415f
CrocutaLupus <luclegrandch@gmail.com>
2020-08-11 08:23:42 +0200

    test.function

--- R/test_function.R
+++ R/test_function.R
@@ -1,5 +1,12 @@
 #' Test Function
 #'
+#'#'This function creates a table with the number of event for a given species.
+#'Input needed:
+#'    - KORA.Photo.Output = "Nameofcsv.csv" KORA ouput with lynx or any other species observed (All have been IDed)
+#'    Important Note:
+#'    - The csv must be in the folder of the used RStudio Project
+#'    - Date in the csv must be in '%Y-%m-%d %H:%M:%S' (Standard KORA data and R format)
+#'
 #' @param # Author: Luc Le Grand
 #'
 #' @return
