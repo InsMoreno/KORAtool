2aec727e0f17b316d7e9b400afc3750c5bb19d37
Luc Le Grand <luclegrandch@gmail.com>
2021-08-11 14:28:42 +0200

    Update Lynx_FirstLast.R
    as.character() in loop

--- R/Lynx_FirstLast.R
+++ R/Lynx_FirstLast.R
@@ -19,12 +19,11 @@ Lynx_FirstLast<-function(LynxMaster=LynxMaster,
   LynxMaster<-LynxMaster[,c("name","coatPattern","yearOfBirth","deathDate","mother")]
 #Fill in info First/Last seen: 
  for(i in 1:length(LynxMaster$name)){
-   LynxMaster[i,"FirstSeen"]<-suppressWarnings(min(min(as.POSIXct(LynxObs[LynxObs$lynx_ID==LynxMaster[i,"name"],"date"],format='%Y-%m-%d',tz="")),
-                                   min(as.POSIXct(PhotoPredator[PhotoPredator$id_individual==LynxMaster[i,"name"],"exposure_date"],format='%Y-%m-%d'))))
-   LynxMaster[i,"LastSeen"]<- suppressWarnings(max(max(as.POSIXct(LynxObs[LynxObs$lynx_ID==LynxMaster[i,"name"],"date"],format='%Y-%m-%d',tz="")),
-                                   max(as.POSIXct(PhotoPredator[PhotoPredator$id_individual==LynxMaster[i,"name"],"exposure_date"],format='%Y-%m-%d'))))
-
- }
+   LynxMaster[i,"FirstSeen"]<-suppressWarnings(min(min(as.POSIXct(LynxObs[LynxObs$lynx_ID==as.character(LynxMaster[i,"name"]),"date"],format='%Y-%m-%d',tz="")),
+                                   min(as.POSIXct(PhotoPredator[PhotoPredator$id_individual==as.character(LynxMaster[i,"name"]),"exposure_date"],format='%Y-%m-%d'))))
+   LynxMaster[i,"LastSeen"]<- suppressWarnings(max(max(as.POSIXct(LynxObs[LynxObs$lynx_ID==as.character(LynxMaster[i,"name"]),"date"],format='%Y-%m-%d',tz="")),
+                                   max(as.POSIXct(PhotoPredator[PhotoPredator$id_individual==as.character(LynxMaster[i,"name"]),"exposure_date"],format='%Y-%m-%d'))))
+}
 #Call Table
 LynxMaster
 }
