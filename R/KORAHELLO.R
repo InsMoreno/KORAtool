3ca6bcb888ccc109520f2733eabbcb399e425681
Luc Le Grand <luclegrandch@gmail.com>
2021-08-13 07:58:15 +0200

    Update KORAHELLO.R

--- R/KORAHELLO.R
+++ R/KORAHELLO.R
@@ -5,5 +5,8 @@
 #' @return text
 #' @export$
 KORAHELLO <- function(Username) {
+  
+#This is a simple function I use to illustrate the package's functions.
 print(paste("Hello ", Username, ", the current time is: ",Sys.time(),sep=""))
+  
 }
