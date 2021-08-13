fc570f00fe0424341e66457d410238d2697c2e98
Luc Le Grand <luclegrandch@gmail.com>
2021-08-13 09:30:14 +0200

    Update KORAmapopp.R
    Change order plotting/subsetting U pts

--- R/KORAmapopp.R
+++ R/KORAmapopp.R
@@ -83,13 +83,6 @@ KORAmapopp<-function(LynxObs,Start,Stop,Compartment,Refarea,IDremove,Buffer.poly
     ggplot2::geom_sf(data=Lakes,fill="#56B4E9")+
     #Reference Area
     ggplot2::geom_sf(data=Rcompartment,col="#009E73",fill=NA,lwd=1.1)
-  
-  # -- Add Points where U were seen
-
-  #Subset "U"
-  pts<- pts %>% dplyr::filter(lynx_ID=="U")
-  #Add Points
-  map<-map+ggplot2::geom_sf(data = pts,col="#D55E00",size=3)
  
   # --- Add the polygons and labels: ####
   
@@ -158,7 +151,14 @@ KORAmapopp<-function(LynxObs,Start,Stop,Compartment,Refarea,IDremove,Buffer.poly
     data_labels[i, "col.ID"]<- ID.names[i,col]
   }
   
-  # -- Add to the map
+  # -- Add Points where U were seen
+
+  #Subset "U"
+  pts<- pts %>% dplyr::filter(lynx_ID=="U")
+  #Add Points
+  map<-map+ggplot2::geom_sf(data = pts,col="#D55E00",size=3)
+  
+  # -- Add Polygons to the map
   
   for(i in 1:length(ID.names$ID)){
     
